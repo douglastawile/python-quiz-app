09927e8cec02f4ea432d0caa6016b3b26c5bc014
Douglas Tawile(Code Whisperer) <49791450+douglastawile@users.noreply.github.com>
2026-07-03 20:32:26 +0000

    feat: create initial console quiz application

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("QUIZ APPLICATION")
+print("Welcome to the Python Quiz!")
+
+answer = input("What is the capital of Ghana?")
+
+if answer.lower() == "accra":
+  print("Correct")
+else:
+  print("Wrong! The correct answer is Accra.")
