9bd2b60955ef41f8a0e894dc9055d30586ec1748
Douglas Tawile(Code Whisperer) <49791450+douglastawile@users.noreply.github.com>
2026-07-03 21:34:43 +0000

    feat: add support for multiple quiz questions

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-print("Welcome to the Python Quiz!")
+print("=" * 30)
+print("WELCOME TO THE QUIZ APP 📘📗📚")
+print("=" * 30)
 
-answer = input("What is the capital of Ghana?")
+questions = [
+    "What is the capital of Ghana?",
+    "What is 5 + 5?",
+    "What color is the sky?"
+]
 
-if answer.lower() == "accra":
-  print("Correct")
-else:
-  print("Wrong! The correct answer is Accra.")
+answers = [
+    "accra",
+    "10",
+    "blue"
+]
+
+score = 0
+
+for question in range(len(questions)):
+    print(f"\nQuestion {question + 1}")
+
+    user_answer = input(questions[question] + " ")
+
+    if user_answer.lower() == answers[question]:
+        print("✅ Correct!")
+        score += 1
+    else:
+        print("❌ Wrong!")
+
+print("\nQuiz Completed!")
+print(f"Your Score is: {score}/{len(questions)}")
