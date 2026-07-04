c61fb05a552dd916fcc60990d4b0affee3377eec
Douglas Tawile(Code Whisperer) <49791450+douglastawile@users.noreply.github.com>
2026-07-04 22:12:00 +0000

    feat: implemented percentage, compare grades and remark for quiz app

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ quiz = [
     {
         "question": "What color is the sky?",
         "answer": "blue"
+    },
+    {
+        "question": "Who is the first man God Created?",
+        "answer": "adam"
+    },
+    {
+        "question": "What is 10 x 5",
+        "answer": "50"
     }
 ]
 
@@ -27,8 +35,6 @@ for item in quiz:
 
     user_answer = input(item["question"] + " ")
 
-    question_number += 1
-
     if user_answer.lower() == item["answer"]:
         print("✅ Correct!")
         score += 1
@@ -36,5 +42,40 @@ for item in quiz:
         print("❌ Wrong!")
         print(f"Correct Answer: {item['answer'].capitalize()}")
 
-print("\nQuiz Completed!")
+    question_number += 1
+
+percentage = (score / len(quiz)) * 100
+
+if percentage >= 80:
+    grade = "A"
+elif percentage >= 70:
+    grade = "B"
+elif percentage >= 60:
+    grade = "C"
+elif percentage >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+
+match grade:
+    case "A":
+        message = "Excellent 😀"
+    case "B":
+        message = "Very Good 😋"
+    case "C":
+        message = "Good Job 👏"
+    case "D":
+        message = "Keep Practicing 😉"
+    case _:
+        message = "Try Again 🙁"
+
+
+print("\n" + "=" * 40)
+print("QUIZ RESULTS")
+print("=" * 40)
+
 print(f"Your score is {score} / {len(quiz)}")
+print(f"Your Percentage is: {percentage:.1f}%")
+print(f"Your Grade is: {grade}")
+print(message)
